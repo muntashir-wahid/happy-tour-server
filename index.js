1d18aa1497d87f66c541db309f2cda9bd1135acc
muntashir-wahid <muntashir.wahid@yahoo.com>
2022-10-22 21:08:04 +0600

    created two simple api to serve data about some travel package

--- index.js
+++ index.js
@@ -4,10 +4,22 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const allPackages = require("./data/tour-package-data.json");
+
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello from Happy Tour server");
+});
+
+app.get("/packages", (req, res) => {
+  res.send(allPackages);
+});
+
+app.get("/package/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedPackage = allPackages.find((package) => package.id === id);
+  res.send(selectedPackage);
 });
 
 app.listen(port, () => {
